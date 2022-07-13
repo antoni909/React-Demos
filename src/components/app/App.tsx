699b9ffefe1a36f5f23bc56d21586ab12e0cd56c
antoni <antoni.dev0@gmail.com>
2022-07-13 16:16:05 -0700

    main br first commit, init basic react ts project, prune project template

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import Header from '../header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>App Component</h1>
+    </div>
+  );
+}
+
+export default App;
