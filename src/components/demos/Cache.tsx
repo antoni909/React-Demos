641a4a51a739809465369dd86e7a431dd8f4632b
antoni <antoni.dev0@gmail.com>
2022-07-14 12:20:02 -0700

    add starter logic for cache object, implement cache with useEffect, and render the cache, selection menu, results

--- src/components/demos/Cache.tsx
+++ src/components/demos/Cache.tsx
@@ -1,24 +1,41 @@
 import './Cache.css'
 import { useState, useEffect } from 'react'
 
-// TODO: find better type solution for data's useState()
+// TODO: find better type solution for data's type useState()
+// TODO: index the cache object with helper function hasKey()
+// TODO: find better type solution for selectionHandler event type
+
+type CacheObject = { [query:string]: string }
+const cache: CacheObject = { }
 
 const Cache = () => {
 
-    const [ status, setStatus ] = useState<string>('idle')
+    const [ status, setStatus ] = useState<string>('idle :|')
     const [ data, setData ] = useState<any[]>([])
     const [ query, setQuery ] = useState<string>('people')
+    
+    const selectionHandler = (e:any ) => {
+        const selectedValue = e.target.value
+        setQuery( updateQuery => updateQuery = selectedValue )
+    }
 
     useEffect(()=>{
         const fetchData = async () =>{
-
+            setStatus( stats => stats = 'fetching ... ' )
             if(!query) console.log('query is not falsy === ',!query)
-
-            setStatus( stats => stats = 'fetching' )
-            const body = await (await fetch(`https://swapi.dev/api/${query}/`)).json()
-            const { results } = body
-            setData( updateData => updateData = results )
-            setStatus( stats => stats = 'fetched' )
+            if( cache[query] ){
+                const data:any = cache[query]
+                setData( updateData => updateData = data )
+                setStatus( stats => stats = 'cache hit 😀 ' )
+            }
+            else{
+                setStatus( stats => stats = 'cache miss 😩 ' )
+                const body = await (await fetch(`https://swapi.dev/api/${query}`)).json()
+                const { results } = body
+                cache[query] = results
+                setData( updateData => updateData = results )
+                setStatus( stats => stats = 'fresh fetch 😎' )
+            }
         }
         fetchData()
     },[query])
@@ -26,8 +43,29 @@ const Cache = () => {
     return (
         <div className="cache-wrapper">
             <h3>Cache Component</h3>
-            <p>this component fetches Star Wars API data using React's useEffect hook</p>
-            <h4> Status: { status } </h4>
+            <p>This component fetches Star Wars API data using React's useEffect hook</p>
+            <p>Pick a resource from the list of available Star Wars API resources:</p>
+            <form onChange={ selectionHandler } >
+                <label htmlFor="cache-resources"> query </label>
+                <select 
+                    id="cache-resources"
+                    name="select resource" 
+                >
+                    <option value="people">People</option>
+                    <option value="starships">Starships</option>
+                    <option value="vehicles">Vehicles</option>
+                    <option value="species">Species</option>
+                    <option value="planets">Planets</option>
+                </select>
+            </form>
+            <h3> Status: { status } </h3>
+            <h3> Cache: { Object.keys(cache).map( el =>( 
+                     <div key={ el }>
+                                <h5>{ el }</h5>
+                            </div>
+                        ))}
+            </h3>
+            <h3>Results</h3>
             {
                 data.length && data.map( person =>(
                     <div
