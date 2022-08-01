296c07316a4b715c0bf9d19cf9ff85f4d41bb668
antoni <antoni.dev0@gmail.com>
2022-07-31 21:15:49 -0700

    add basic css component logic for Login Component

--- src/components/demos/login/Login.tsx
+++ src/components/demos/login/Login.tsx
@@ -1,36 +1,50 @@
 import './Login.css'
 import { TextField } from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
 
 const Login = () => {
+
+    const handler = (e:any) => {
+        e.preventDefault()
+        const username:string = e.target.username.value
+        const password:string = e.target.password.value
+        console.log(username, password)
+    }
+
+    const handleRemember = () => {
+        console.log('checked')
+    }
 
     return(
         <div className="login-wrapper">
             <h2>Welcome</h2>
             <p>Login by entering the required information</p>
             <div className="login-container">
-                <div className="login-item">
-                    <TextField
-                        label="username"
-                        type="text"
-                        variant='standard'
-                        required
-                    />
-                </div>
-                <div className="login-item">
-                    <TextField
-                        label="password"
-                        type="text"
-                        variant='standard'
-                        required
-                    />
-                </div>
-                <div className="login-item-btn">
-                    <button className='login-item-btn'>Login</button>
-                </div>
+                <form onSubmit={ handler }>
+                    <div className="login-item">
+                        <TextField
+                            label="username"
+                            name="username"
+                            type="text"
+                            variant='standard'
+                            required
+                        />
+                    </div>
+                    <div className="login-item">
+                        <TextField
+                            label="password"
+                            name="password"
+                            type="text"
+                            variant='standard'
+                            required
+                        />
+                    </div>
+                    <div className="login-item-btn">
+                        <button type='submit' className='login-item-btn btn'>Login</button>
+                    </div>
+                </form>
                 <div className="login-item-chkbox">
-                    <Checkbox/> Remember Me?
+                    <Checkbox onClick={ handleRemember }/> Remember Me?
                 </div>
                 <div className="login-item-signup">Not a Member?<a href="#blank">Sign Up</a></div>
             </div>
